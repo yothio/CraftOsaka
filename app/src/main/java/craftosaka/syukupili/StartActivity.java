6ac669c75323e5f93f88110b55d098535721f6fa
shiota yoshiki <shiotyan317@gmail.com>
2017-08-13 16:58:58 +0900

    master test commit

--- app/src/main/java/craftosaka/syukupili/StartActivity.java
+++ app/src/main/java/craftosaka/syukupili/StartActivity.java
@@ -2,6 +2,9 @@ package craftosaka.syukupili;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -9,5 +12,7 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        addContentView(new TextView(this), layoutParams);
     }
 }
