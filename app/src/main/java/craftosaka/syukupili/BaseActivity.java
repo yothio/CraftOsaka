4d9286717a00a12033fc561db54904dbc821782e
shiota yoshiki <shiotyan317@gmail.com>
2017-08-13 16:52:14 +0900

    test branch first commit

--- app/src/main/java/craftosaka/syukupili/BaseActivity.java
+++ app/src/main/java/craftosaka/syukupili/BaseActivity.java
@@ -1,13 +1,15 @@
 package craftosaka.syukupili;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class StartActivity extends BaseActivity {
+/**
+ * Created by yocchi on 2017/08/13.
+ */
 
+public abstract class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start);
     }
 }
